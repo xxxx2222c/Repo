5d8f3fd1a34ed4e593229edaa5280ef134631682
xxxx2222c <xxxx2222cc@gmail.com>
2014-02-17 21:36:05 +0800

    七言絕句
    七言絕句

---
+++ week1.java
@@ -0,0 +1,8 @@
+class Hello1 {
+  public static void main(String[] args)  {
+    System.out.println("少小離家老大回\n");
+    System.out.println("鄉音無改鬢毛摧\n");
+    System.out.println("兒童相見不相識\n");
+    System.out.println("笑問客從何處來\n");
+  }
+}
